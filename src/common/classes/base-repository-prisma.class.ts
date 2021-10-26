ab1cb99d4df9f7d591e0b4f66a127791c16f2428
Edmar <edsiqueira73@gmail.com>
2021-10-25 23:44:50 -0300

    feat: add base repository global classes

---
+++ src/common/classes/base-repository-prisma.class.ts
@@ -0,0 +1,45 @@
+import { Inject, Injectable, Type } from '@nestjs/common'
+import { plainToClass } from 'class-transformer'
+import { AllPrismaModels, AllPrismaWhereInput, PrismaTables } from '@common/types'
+import { PrismaService } from '@modules/prisma'
+import { BaseRepository } from './base-repository.class'
+
+@Injectable()
+export abstract class BaseRepositoryPrisma<T, U extends PrismaTables> extends BaseRepository<T> {
+	@Inject()
+	protected readonly prismaService: PrismaService
+
+	constructor(protected readonly Model: Type<T>, protected readonly table: U) {
+		super()
+	}
+
+	protected async exists(where?: AllPrismaWhereInput[U]): Promise<boolean> {
+		const count = await this.prismaService[this.table].count({
+			where
+		})
+		return count > 0
+	}
+
+	async getById(id: number): Promise<T | null> {
+		// @ts-ignore
+		const result = await this.prismaService[this.table].findUnique({
+			where: {
+				id
+			}
+		})
+		return this.format(result)
+	}
+
+	async existsById(id: number): Promise<boolean> {
+		const exists = await this.exists({ id })
+		return exists
+	}
+
+	protected format<V extends AllPrismaModels[U] | null>(obj: V): V extends null ? null : T {
+		if (!obj) {
+			return null as V extends null ? null : T
+		}
+
+		return plainToClass(this.Model, obj) as V extends null ? null : T
+	}
+}
---
+++ src/common/classes/index.ts
@@ -0,0 +1,2 @@
+export * from './base-repository.class'
+export * from './base-repository-prisma.class'
